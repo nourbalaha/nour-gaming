664e7470903606e10b5a1bffc733278d5d39c2ba
Nour Balaha <nourbalaha0@gmail.com>
2019-10-21 09:57:18 +0900

    created HomePage component

---
+++ src/homepage.component.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+            <div className="directory-menu">
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Consoles</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">PS4</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Switch</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">XBOX One</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">PC</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
