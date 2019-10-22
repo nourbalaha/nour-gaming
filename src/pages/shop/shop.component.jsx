8c87bf8f66245b6d95797752deb3fd1a2d2ed688
Nour Balaha <nourbalaha0@gmail.com>
2019-10-22 18:55:29 +0900

    wip shop page

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+constructor(props) {
+    super(props)
+
+    this.state = {
+         collections: []
+    }
+}
+
+
+export default class shop.component extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
