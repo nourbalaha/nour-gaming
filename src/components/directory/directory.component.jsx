c5daf6561a1298f37aa0fb10a5d500a5eba9c332
Nour Balaha <nourbalaha0@gmail.com>
2019-10-22 16:35:16 +0900

    wip: routing

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -14,33 +14,38 @@ export default class Directory extends Component {
           title: 'Consoles',
           imageUrl:
             'https://cdn.wccftech.com/wp-content/uploads/2018/05/WCCFcontrollers2-740x429.jpg',
-          id: 1
+          id: 1,
+          linkUrl: "consoles"
         },
         {
           title: 'PS4',
           imageUrl:
             'https://cnet1.cbsistatic.com/img/yPzY4G61oVgInoMuUPNpjZx_-zA=/868x488/2013/11/11/b8024faa-67c2-11e3-a665-14feb5ca9861/Sony_PS4_35618167_01.jpg',
-          id: 2
+          id: 2,
+          linkUrl: "ps4"
         },
         {
           title: 'Switch',
           imageUrl:
             'https://cdn.arstechnica.net/wp-content/uploads/2019/07/2-4.jpg',
-          id: 3
+          id: 3,
+          linkUrl: "switch"
         },
         {
           title: 'Xbox One',
           imageUrl:
             'https://cdn.pocket-lint.com/r/s/970x/assets/images/141329-games-review-xbox-one-x-review-image2-28fwsqx4oi.jpg',
           size: 'large',
-          id: 4
+          id: 4,
+          linkUrl: "xbox-one"
         },
         {
           title: 'PC',
           imageUrl:
             'https://icdn5.digitaltrends.com/image/gaming-pc-2-1050x591.jpg',
           size: 'large',
-          id: 5
+          id: 5,
+          linkUrl: "pc"
         }
       ]
     }
@@ -48,8 +53,8 @@ export default class Directory extends Component {
   render () {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({ id, imageUrl, title, size }) =>
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, imageUrl, title, size, linkUrl }) =>
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl} />
         )}
       </div>
     )
