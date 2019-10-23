a8a19f3a77b18e84a20802ca24a3b33fa9cac95c
Nour Balaha <nourbalaha0@gmail.com>
2019-10-23 15:43:22 +0900

    crated sign in and sign up page

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import "./sign-in-and-sign-up.styles.scss"
+
+const SignInAndSignUpPage = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SignInAndSignUpPage
