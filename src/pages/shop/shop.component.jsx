606189277ac7c013fdb9953d97eccaebcad34b54
Nour Balaha <nourbalaha0@gmail.com>
2019-10-23 10:35:39 +0900

    wip collection item

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,19 +1,24 @@
 import React, { Component } from 'react'
+
 import SHOP_DATA from "./shop.data";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component"
 
 export default class ShopPage extends Component {
     constructor(props) {
         super(props)
     
         this.state = {
-             collections: [...SHOP_DATA]
+             collections: SHOP_DATA
         }
     }
     render() {
-        return (
-            <div>
-                
-            </div>
-        )
+        const { collections } = this.state;
+        return (<div className="shop-page">
+                {
+                    collections.map(({id, ...otherCollectionProps }) => (
+                        <CollectionPreview key={id} {...otherCollectionProps} />
+                    ))
+                }
+            </div>)
     }
 }
