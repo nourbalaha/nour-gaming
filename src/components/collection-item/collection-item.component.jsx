9a432c1829faaa533fae04404fca2f109f18f1ca
Nour Balaha <nourbalaha0@gmail.com>
2019-10-23 10:49:32 +0900

    wip shop

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -8,7 +8,7 @@ export default function CollectionItem({ id, name, price, imageUrl }) {
             <div className="image" style={{backgroundImage: `url(${imageUrl})`}} />
             <div className="collection-footer">
                 <span className="name">{ name }</span>
-                <span className="name">{ price }</span>
+                <span className="price">{ price }</span>
             </div>
         </div>
     )
