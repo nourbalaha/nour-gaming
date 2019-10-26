f800b37f04d45ffa9f742b9ff356df8140d7c544
Nour Balaha <nourbalaha0@gmail.com>
2019-10-26 20:15:27 +0900

    wip signup

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -18,6 +18,39 @@ export default class SignUp extends Component {
              confirmPassword: ""
         }
     }
+
+    handleSubmit = async event => {
+        event.preventDefault();
+
+        const { displayName, email, password, confirmPassword } = this.state;
+
+        if(password!== confirmPassword){
+            alert("passwords don't match");
+            return;
+        }
+
+        try {
+            const { user } = await auth.createUserWithEmailAndPassword(email, password);
+
+            await createUserProfileDocument(user, {displayName});
+
+            this.setState({
+                displayName: "",
+                email: "",
+                password: "",
+                confirmPassword: ""
+            })
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    handleChange = event => {
+        const { name, value } = event.target;
+
+        this.setState({ [name]: value })
+    }
     
     render() {
         return (
