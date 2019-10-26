e2dd30cdc77551d9f0381b10983122ac7341b2e3
Nour Balaha <nourbalaha0@gmail.com>
2019-10-26 22:17:53 +0900

    finished sign up

--- src/App.js
+++ src/App.js
@@ -32,9 +32,11 @@ class App extends React.Component{
               id: snapShot.id,
             ...snapShot.data()
             }
+          },()=>{
+
+            console.log(this.state)
           })
         })
-
       }
 
       this.setState({ currentUser: userAuth })
--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -58,10 +58,10 @@ export default class SignUp extends Component {
                 <h2 className="title">I do not have an account</h2>
                 <span>Sign up with your email and password</span>
                 <form className="sign-up-form" onSubmit={this.handleSubmit}>
-                    <FormInput type="text" name="displayName" value={displayName} onChange={this.handleChange} label="Display Name" required />
-                    <FormInput type="email" name="email" value={displayName} onChange={this.handleChange} label="Email" required />
-                    <FormInput type="password" name="password" value={displayName} onChange={this.handleChange} label="Password" required />
-                    <FormInput type="password" name="confirmPassword" value={displayName} onChange={this.handleChange} label="Confirm Password" required />
+                    <FormInput type="text" name="displayName" value={this.state.displayName} onChange={this.handleChange} label="Display Name" required />
+                    <FormInput type="email" name="email" value={this.state.email} onChange={this.handleChange} label="Email" required />
+                    <FormInput type="password" name="password" value={this.state.password} onChange={this.handleChange} label="Password" required />
+                    <FormInput type="password" name="confirmPassword" value={this.state.confirmPassword} onChange={this.handleChange} label="Confirm Password" required />
                     <CustomButton type="submit">Sign Up</CustomButton>
                 </form>
             </div>
