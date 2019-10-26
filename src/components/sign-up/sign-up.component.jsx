01cb44b298c52eda0277ec5b2e599520887952c1
Nour Balaha <nourbalaha0@gmail.com>
2019-10-26 20:06:33 +0900

    wip sign up component

---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
+
+import { auth, createUserProfileDocument } from "../../firebase/firebase.utils";
+
+import "./sign-up.styles.scss";
+
+export default class SignUp extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             displayName: "",
+             email: "",
+             password: "",
+             confirmPassword: ""
+        }
+    }
+    
+    render() {
+        return (
+            <div className="sign-up">
+                <h2 className="title">I do not have an account</h2>
+                <span>Sign up with your email and password</span>
+                <form className="sign-up-form" onSubmit={this.handleSubmit}>
+                    <FormInput type="text" name="displayName" value={displayName} onChange={this.handleChange} label="Display Name" required />
+                    <FormInput type="email" name="email" value={displayName} onChange={this.handleChange} label="Email" required />
+                    <FormInput type="password" name="password" value={displayName} onChange={this.handleChange} label="Password" required />
+                    <FormInput type="password" name="confirmPassword" value={displayName} onChange={this.handleChange} label="Confirm Password" required />
+                    <CustomButton type="submit">Sign Up</CustomButton>
+                </form>
+            </div>
+        )
+    }
+}
