06de30ebeef87038bbbea8a5f2c9bc0cc0cc1582
Nour Balaha <nourbalaha0@gmail.com>
2019-11-02 16:09:33 +0900

    added cart icon with styles

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { connect } from "react-redux"
 
 import { auth } from "../../firebase/firebase.utils"
+import CartIcon from "../cart-icon/cart-icon.component"
 
 import { ReactComponent as Logo } from '../../assets/game.svg'
 
@@ -27,6 +28,7 @@ function Header ({ currentUser }) {
           :
           <Link className="option" to="/signin">SIGN IN</Link>
         }
+        <CartIcon />
       </div>
     </div>
   )
