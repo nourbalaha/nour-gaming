adfedbfd6a8ed010a1f8cdc69b5c67ebdf86a2c1
Nour Balaha <nourbalaha0@gmail.com>
2019-11-02 17:55:22 +0900

    added cart dropdown component and cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -10,7 +10,7 @@ import { ReactComponent as Logo } from '../../assets/game.svg'
 
 import './header.styles.scss'
 
-function Header ({ currentUser }) {
+function Header ({ currentUser, hidden }) {
   return (
     <div className='header'>
       <Link to='/' className='logo-container'>
@@ -31,13 +31,16 @@ function Header ({ currentUser }) {
         }
         <CartIcon />
       </div>
-      <CartDropdown />
+      {
+        hidden ? null : <CartDropdown />
+      }
     </div>
   )
 }
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden }}) => ({
+  currentUser,
+  hidden
 })
 
 export default connect(mapStateToProps)(Header);
